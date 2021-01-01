8d7be067d13e049bd76aee4196949a6c7fdffc42
Mateusz <mateusz.pytel@grupakmk.pl>
2021-01-01 20:35:09 +0100

    dist

--- dist/index.js
+++ dist/index.js
@@ -434,6 +434,17 @@ exports.orderReturnsReducer = orderReturnsReducer;
 
 "use strict";
 
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -543,10 +554,15 @@ var OrderReturnsThunks;
         });
     }); }; };
     OrderReturnsThunks.createOrderReturn = function (orderReturnData) { return function (dispatch, getState) { return __awaiter(_this, void 0, void 0, function () {
-        var response;
+        var customer, response;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, libstorefront_1.IOCContainer.get(dao_1.OrderReturnsDao).createReturn(orderReturnData)];
+                case 0:
+                    customer = libstorefront_1.IOCContainer.get(libstorefront_1.AbstractStore).getState().user.current;
+                    if (!customer) {
+                        throw new Error('Cannot create order returns for unauthorized used');
+                    }
+                    return [4 /*yield*/, libstorefront_1.IOCContainer.get(dao_1.OrderReturnsDao).createReturn(__assign(__assign({}, orderReturnData), { customer_id: customer.id, customer_email: customer.email }))];
                 case 1:
                     response = _a.sent();
                     return [2 /*return*/, response];
