9f714169977f9781e624df949295f5c202f79812
Mateusz <mateusz.pytel@grupakmk.pl>
2021-01-04 13:28:36 +0100

    OrderReturn type fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,13 @@
+import { CartItem } from '@grupakmk/libstorefront';
+
 export interface OrderReturn {
     order_id: number,
     customer_id?: string,
     customer_email?: string,
     reason: string,
     comment: string,
-    items: unknown[],
-    return_id?: number,
+    items: CartItem[],
+    returns_id?: number,
     created_at?: string,
     status?: number,
     return_number?: string,
